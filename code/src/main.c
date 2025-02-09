495bcae877423e961b0f7731764539f321a4bf97
Lucas-HGS-Official <lucas.henrique.gois.santos@gmail.com>
2025-02-09 20:17:48 -0300

    making the SDL_Window into a local variable

--- code/src/main.c
+++ code/src/main.c
@@ -3,28 +3,29 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
-SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
 uint32_t* colorBuffer = NULL;
 
 SDL_Texture* colorBufferTexture = NULL;
 
-bool initialize_window(int windowWidth, int windowHeight);
+bool initialize_window(int windowWidth, int windowHeight, SDL_Window** window);
 void setup(int windowWidth, int windowHeight);
 void process_input(bool* isRunning);
 void update(void);
 void render_color_buffer(int windowWidth);
 void clearColorBuffer(int windowWidth, int windowHeight, uint32_t color);
 void render(int windowWidth, int windowHeight);
-void destroy_window(void);
+void destroy_window(SDL_Window** window);
 
 int main(int argc, char const *argv[]) {
+    SDL_Window* window = NULL;
+
     int windowWidth = 800;
     int windowHeight = 600;
     bool isRunning = false;
 
-    isRunning = initialize_window(windowWidth, windowHeight);
+    isRunning = initialize_window(windowWidth, windowHeight, &window);
 
     setup(windowWidth, windowHeight);
 
@@ -34,12 +35,12 @@ int main(int argc, char const *argv[]) {
         render(windowWidth, windowHeight);
     }
 
-    destroy_window();
+    destroy_window(&window);
 
     return 0;
 }
 
-bool initialize_window(int windowWidth, int windowHeight) {
+bool initialize_window(int windowWidth, int windowHeight, SDL_Window** window) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         fprintf(stderr, "Error initializing SDL.\n");
         
@@ -51,26 +52,26 @@ bool initialize_window(int windowWidth, int windowHeight) {
     windowWidth = displayMode.w;
     windowHeight = displayMode.h;
 
-    window = SDL_CreateWindow(
+    *window = SDL_CreateWindow(
         NULL,
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         windowWidth, windowHeight,
         SDL_WINDOW_BORDERLESS
     );
-    if(!window) {
+    if(!*window) {
         fprintf(stderr, "Error creating the SDL Window.\n");
 
         return false;
     }
 
-    renderer = SDL_CreateRenderer(window, -1, 0);
+    renderer = SDL_CreateRenderer(*window, -1, 0);
     if (!renderer) {
         fprintf(stderr, "Error creating SDL renderer.\n");
 
         return false;
     }
 
-    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+    SDL_SetWindowFullscreen(*window, SDL_WINDOW_FULLSCREEN);
 
     return true;
 }
@@ -137,9 +138,9 @@ void render(int windowWidth, int windowHeight) {
     SDL_RenderPresent(renderer);
 }
 
-void destroy_window() {
+void destroy_window(SDL_Window** window) {
     free(colorBuffer);
     SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow(*window);
     SDL_Quit();
 }
