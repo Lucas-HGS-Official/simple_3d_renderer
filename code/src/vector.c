cb454e24d730b5e1d7a989df4469edb737262ca4
Lucas-HGS-Official <lucas.henrique.gois.santos@gmail.com>
2025-02-17 11:58:00 -0300

    Small bug correction

--- code/src/vector.c
+++ code/src/vector.c
@@ -1,19 +1,22 @@
 #include <math.h>
 
 #include "vector.h"
+
 vec3_t vec3RotateX(vec3_t v, float angle) {
     vec3_t rotatedVector = {
-        v.x,
-        v.x * cos(angle) - v.y * sin(angle),
-        v.z * sin(angle) + v.y * cos(angle)
+        .x = v.x,
+        .y = v.y * cos(angle) - v.z * sin(angle),
+        .z = v.y * sin(angle) + v.z * cos(angle)
     };
 
     return rotatedVector;
-}vec3_t vec3RotateY(vec3_t v, float angle) {
+}
+
+vec3_t vec3RotateY(vec3_t v, float angle) {
     vec3_t rotatedVector = {
-        v.x * cos(angle) - v.y * sin(angle),
-        v.x,
-        v.z * sin(angle) + v.y * cos(angle)
+        .x = v.x * cos(angle) - v.z * sin(angle),
+        .y = v.y,
+        .z = v.x * sin(angle) + v.z * cos(angle)
     };
 
     return rotatedVector;
@@ -21,9 +24,9 @@ vec3_t vec3RotateX(vec3_t v, float angle) {
 
 vec3_t vec3RotateZ(vec3_t v, float angle) {
     vec3_t rotatedVector = {
-        v.x * cos(angle) - v.y * sin(angle),
-        v.x * sin(angle) + v.y * cos(angle),
-        v.z
+        .x = v.x * cos(angle) - v.y * sin(angle),
+        .y = v.x * sin(angle) + v.y * cos(angle),
+        .z = v.z
     };
 
     return rotatedVector;
