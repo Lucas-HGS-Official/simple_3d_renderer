1cd11d41996d1f320c257684bdd683ee307af395
Lucas-HGS-Official <lucas.henrique.gois.santos@gmail.com>
2025-02-20 10:00:03 -0300

    Deleting cube especific code

--- code/src/main.c
+++ code/src/main.c
@@ -6,10 +6,6 @@
 #include "display.h"
 #include "vector.h"
 
-#define N_POINTS (9 * 9 * 9)
-
-vec3_t cubePoints[N_POINTS];
-vec2_t projectedPoints[N_POINTS];
 vec3_t cubeRotation = { 0, 0, 0 };
 
 vec3_t cameraPos = { 0, 0, -5 };
@@ -51,17 +47,6 @@ void setup(int windowWidth, int windowHeight, SDL_Renderer** renderer) {
         windowWidth,
         windowHeight
     );
-
-    int pCount = 0;
-
-    for (float x = -1; x <= 1; x += 0.25) {
-        for (float y = -1; y <= 1; y += 0.25) {
-            for (float z = -1; z <= 1; z += 0.25) {
-                vec3_t newPoint = { x, y, z };
-                cubePoints[pCount++] = newPoint;
-            }
-        }
-    }
 }
 
 void processInput(bool* isRunning) {
@@ -101,32 +86,32 @@ void update() {
     cubeRotation.y += 0.005;
     cubeRotation.z += 0.005;
 
-    for (int i = 0; i < N_POINTS; i++) {
-        vec3_t point = cubePoints[i];
+    // for (int i = 0; i < N_POINTS; i++) {
+    //     vec3_t point = cubePoints[i];
 
-        vec3_t transformedPoint = vec3RotateX(point, cubeRotation.x);
-        transformedPoint = vec3RotateY(transformedPoint, cubeRotation.y);
-        transformedPoint = vec3RotateZ(transformedPoint, cubeRotation.z);
+    //     vec3_t transformedPoint = vec3RotateX(point, cubeRotation.x);
+    //     transformedPoint = vec3RotateY(transformedPoint, cubeRotation.y);
+    //     transformedPoint = vec3RotateZ(transformedPoint, cubeRotation.z);
 
-        transformedPoint.z -= cameraPos.z;
+    //     transformedPoint.z -= cameraPos.z;
 
-        vec2_t projectedPoint = project(transformedPoint);
+    //     vec2_t projectedPoint = project(transformedPoint);
 
-        projectedPoints[i] = projectedPoint;
-    }
+    //     projectedPoints[i] = projectedPoint;
+    // }
 }
 
 void render() {
     // drawGrid();
 
-    for (int i=0; i<N_POINTS; i++) {
-        vec2_t projectedPoint = projectedPoints[i];
-        drawRectFilled(
-            projectedPoint.x + (windowWidth / 2),
-            projectedPoint.y + (windowHeight / 2),
-            4, 4,
-            0xFFFFFF00);
-    }
+    // for (int i=0; i<N_POINTS; i++) {
+    //     vec2_t projectedPoint = projectedPoints[i];
+    //     drawRectFilled(
+    //         projectedPoint.x + (windowWidth / 2),
+    //         projectedPoint.y + (windowHeight / 2),
+    //         4, 4,
+    //         0xFFFFFF00);
+    // }
 
     renderColorBuffer();
     clearColorBuffer(0xFF000000);
