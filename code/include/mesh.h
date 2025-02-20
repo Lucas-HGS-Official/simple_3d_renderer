24e5e8b852ac94c82c1cb1e7e94352ff9cc816be
Lucas-HGS-Official <lucas.henrique.gois.santos@gmail.com>
2025-02-20 10:10:40 -0300

    Defining the faces for a 3d cube mesh

--- code/include/mesh.h
+++ code/include/mesh.h
@@ -3,6 +3,8 @@
 
 #include "vector.h"
 
+#include "triangle.h"
+
 #define NUM_MESH_VERTICES 8
 vec3_t meshVertices[NUM_MESH_VERTICES] = {
     { -1, -1, -1 }, // 1
@@ -13,6 +15,28 @@ vec3_t meshVertices[NUM_MESH_VERTICES] = {
     {  1, -1,  1 }, // 6
     { -1,  1,  1 }, // 7
     { -1, -1,  1 }  // 8
+};
+
+#define NUM_MESH_FACES (6 * 2)
+face_t meshFaces[NUM_MESH_FACES] = {
+    // front
+    {1, 2, 3},
+    {1, 3, 4},
+    // right
+    {4, 3, 5},
+    {4, 5, 6},
+    // back
+    {6, 5, 7},
+    {6, 7, 8},
+    // left
+    {8, 7, 2},
+    {8, 2, 1},
+    // top
+    {2, 7, 5},
+    {2, 5, 3},
+    // bottom
+    {6, 8, 1},
+    {6, 1, 4}
 };
 
 #endif
