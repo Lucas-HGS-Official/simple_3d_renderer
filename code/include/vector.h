77fd63f2e8afba8cafd1c54cdd0dc25967a42835
Lucas-HGS-Official <lucas.henrique.gois.santos@gmail.com>
2025-02-26 23:48:00 -0300

    Implementing vector addition

--- code/include/vector.h
+++ code/include/vector.h
@@ -13,8 +13,10 @@ typedef struct {
 } vec3_t;
 
 float vec2Length(vec2_t v);
+vec2_t vec2Addition(vec2_t v1, vec2_t v2);
 
 float vec3Length(vec3_t v);
+vec3_t vec3Addition(vec3_t v1, vec3_t v2);
 vec3_t vec3RotateX(vec3_t v, float angle);
 vec3_t vec3RotateY(vec3_t v, float angle);
 vec3_t vec3RotateZ(vec3_t v, float angle);
--- code/src/vector.c
+++ code/src/vector.c
@@ -7,9 +7,17 @@ float vec2Length(vec2_t v) {
     return sqrt(v.x*v.x + v.y*v.y);
 }
 
+vec2_t vec2Addition(vec2_t v1, vec2_t v2) {
+    return (vec2_t) { v1.x + v2.x, v1.y + v2.y };
+}
+
 
 float vec3Length(vec3_t v) {
     return sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+}
+
+vec3_t vec3Addition(vec3_t v1, vec3_t v2){
+    return (vec3_t) { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
 }
 
 vec3_t vec3RotateX(vec3_t v, float angle) {
