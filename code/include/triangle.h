8d2837d7ec4708a68b372163e11456f346258148
Lucas-HGS-Official <lucas.henrique.gois.santos@gmail.com>
2025-03-03 10:06:44 -0300

    Sorting the vertices of triangle by Y values

--- code/include/triangle.h
+++ code/include/triangle.h
@@ -1,6 +1,8 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
+#include<stdint.h>
+
 #include "vector.h"
 
 typedef struct {
@@ -13,5 +15,6 @@ typedef struct {
     vec2_t points[3];
 } triangle_t;
 
+void drawTriangleFilled(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 
 #endif
--- code/src/triangle.c
+++ code/src/triangle.c
@@ -1,2 +1,22 @@
 #include "triangle.h"
 
+void intSwapVertex(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void drawTriangleFilled(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+    if (y0 > y1) {
+        swapVertex(&y0, &y1);
+        swapVertex(&x0, &x1);
+    }
+    if (y1 > y2) {
+        swapVertex(&y1, &y2);
+        swapVertex(&x1, &x2);
+    }
+    if (y0 > y1) {
+        swapVertex(&y0, &y1);
+        swapVertex(&x0, &x1);
+    }
+}
