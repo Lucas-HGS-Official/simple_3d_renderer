f0ca04e100a0ab3acc59d78e25a824457a61bd21
Lucas-HGS-Official <89606089+Lucas-HGS-Official@users.noreply.github.com>
2025-03-04 19:05:28 -0300

    Implementing fillFlatTopTriangle

--- code/src/triangle.c
+++ code/src/triangle.c
@@ -15,14 +15,24 @@ fillFlatBottomTriangle(int x0, int y0, int x1, int y1, int mX, int mY, uint32_t 
     float xStart = x0;
     float xEnd = x0;
 
-    for (int y = 0; y <= mY; y++) {
+    for (int y = y0; y <= mY; y++) {
         drawLine(xStart, y, xEnd, y, color);
         xStart += invSlope1;
         xEnd += invSlope2;
     }
 }
 fillFlatTopTriangle(int x1, int y1, int mX, int mY, int x2, int y2, uint32_t color) {
+    float invSlope1 = (float)(x2 - x1) / (y2 - y1);
+    float invSlope2 = (float)(x2 - mX) / (y2 - mY);
 
+    float xStart = x2;
+    float xEnd = x2;
+
+    for (int y = y2; y >= y1; y--) {
+        drawLine(xStart, y, xEnd, y, color);
+        xStart -= invSlope1;
+        xEnd -= invSlope2;
+    }
 }
 
 void drawTriangleFilled(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
