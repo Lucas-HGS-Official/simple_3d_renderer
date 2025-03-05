bf26ac2ee7dd9dbc24916c37ca90f09bebdcd9d2
Lucas-HGS-Official <lucas.henrique.gois.santos@gmail.com>
2025-03-05 09:14:34 -0300

    Preventing divisions by zero

--- code/src/triangle.c
+++ code/src/triangle.c
@@ -49,9 +49,15 @@ void drawTriangleFilled(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t
         intSwapVertex(&x0, &x1);
     }
 
-    int mY = y1;
-    int mX = ((float)((x2-x0) * (y1-y0)) / (float)(y2-y0)) + x0;
-
-    fillFlatBottomTriangle(x0, y0, x1, y1, mX, mY, color);
-    fillFlatTopTriangle(x1, y1, mX, mY, x2, y2, color);
+    if (y1 == y2) {
+        fillFlatBottomTriangle(x0, y0, x1, y1, x2, y2, color);
+    } else if (y0 == y1) {
+        fillFlatTopTriangle(x0, y0, x1, y1, x2, y2, color);
+    } else {   
+        int mY = y1;
+        int mX = ((float)((x2-x0) * (y1-y0)) / (float)(y2-y0)) + x0;
+        
+        fillFlatBottomTriangle(x0, y0, x1, y1, mX, mY, color);
+        fillFlatTopTriangle(x1, y1, mX, mY, x2, y2, color);
+    }
 }
