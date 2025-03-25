4a7eeea655ce0ddab51a5da1aee6bf637744348e
Lucas-HGS-Official <lucas.henrique.gois.santos@gmail.com>
2025-03-25 10:57:15 -0300

    Implemented the swap files

--- code/src/triangle.c
+++ code/src/triangle.c
@@ -1,18 +1,6 @@
 #include "display.h"
 #include "triangle.h"
 
-void int_swap(int* a, int* b) {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
-void float_swap(float* a, float* b) {
-    float tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-
 ///////////////////////////////////////////////////////////////////////////////
 // Draw a filled a triangle with a flat bottom
 ///////////////////////////////////////////////////////////////////////////////
