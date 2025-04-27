12864b45dfdca772dc2bc458b410cb2913fbc5b5
LhgS <lucas.henrique.gois.santos@gmail.com>
2025-04-27 17:30:14 -0300

    Cleaning up the update

--- code/src/triangle.c
+++ code/src/triangle.c
@@ -363,3 +363,22 @@ void draw_filled_triangle(
         }
     }
 }
+
+vec3_t get_triangle_normal(vec4_t vertices[3]) {
+    // Get individual vectors from A, B, and C vertices to compute normal
+    vec3_t vector_a = vec3_from_vec4(vertices[0]); /*   A   */
+    vec3_t vector_b = vec3_from_vec4(vertices[1]); /*  / \  */
+    vec3_t vector_c = vec3_from_vec4(vertices[2]); /* C---B */
+
+    // Get the vector subtraction of B-A and C-A
+    vec3_t vector_ab = vec3_sub(vector_b, vector_a);
+    vec3_t vector_ac = vec3_sub(vector_c, vector_a);
+    vec3_normalize(&vector_ab);
+    vec3_normalize(&vector_ac);
+
+    // Compute the face normal (using cross product to find perpendicular)
+    vec3_t normal = vec3_cross(vector_ab, vector_ac);
+    vec3_normalize(&normal);
+    
+    return normal;
+}
